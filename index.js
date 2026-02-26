88e773ed2893999340f4a6e385c9dd3d72ed80e3
artin793 <Artin_Morina2008@outlook.com>
2026-02-26 14:08:06 +0100

    La til rute /deltagere-1

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hei verden!</h1>");
 });
 
 app.listen(port, () => {
   console.log(`Server kjører på http://localhost:${port}`);
+});
+
+app.get("/deltagere-1", (req, res) => {
+  res.send(`
+    <h1>Deltagere</h1>
+    <ul>
+      <li>Ola</li>
+      <li>Kari</li>
+      <li>Ali</li>
+      <li>Sara</li>
+    </ul>
+  `);
 });
