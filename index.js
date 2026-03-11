86ba0d3957a8668065016471d73c16e6a85ce73e
artin793 <Artin_Morina2008@outlook.com>
2026-03-11 13:33:45 +0100

    Koblet Express til PostgreSQL og laget users-json route

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const express = require("express");
+const { Pool } = require("pg");
+
 const app = express();
-const port = 3000;
+const port = 3001;
 
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: "mysecretpassword",
+  port: 5432,
+});
+
+// Forside
 app.get("/", (req, res) => {
+  res.send("<h1>Serveren fungerer!</h1>");
 });
 
-app.listen(port, () => {
-  console.log(`Server kjører på http://localhost:${port}`);
+
+// JSON-rute
+app.get("/users-json", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM users");
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Database-feil");
+  }
 });
 
+// Deltagere-rute
 app.get("/deltagere-1", (req, res) => {
   res.send(`
     <h1>Deltagere</h1>
@@ -20,3 +41,13 @@ app.get("/deltagere-1", (req, res) => {
     </ul>
   `);
 });
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+});
+
+// START SERVER (ALLTID NEDERST)
+app.listen(port, () => {
+  console.log(`Server kjører på http://localhost:${port}`);
+});
+
